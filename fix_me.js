e7916bb78e65c6b25332951144a6ff93a8493c07
Ezra Hampton <ezra.hamptoniii@gmail.com>
2022-10-08 20:07:22 -0400

    added some broken code

---
+++ fix_me.js
@@ -0,0 +1,11 @@
+function pleaseWork(arr1, arr2){
+const arr1 = [];
+const arr2 = [];
+
+if(){
+// get the arrays to have some sort of match so you can use one of them to check if the other has the same valuse as the other one. 
+// good luck!
+
+}
+
+}
